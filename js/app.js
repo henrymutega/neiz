df6b18ab74934d60ceb9251b5ed2e85e3f01a528
henrymutega <hmutega@gmail.com>
2019-12-10 13:05:49 +0300

    Add footer section to the project.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,3 @@
-/*************USER INTERFACE*********/
 $(document).ready(function(){
-  $("#sub").click(function(){
-    var name = $("#name").val();
-    var email = $("#mail").val();
-    var comment = $("#comment").val();
-
-  if(name== ""){
-    alert("Please Enter your Name.");
-    return;
-  }else if(email== ""){
-    alert("Please Enter your Email.");
-    return;
-  }else if (comment== "") {
-    alert("Please Enter your Message.");
-    return;
-  }else{
-    alert(name.toUpperCase() + " We have recieved your message. Thank you for reaching out to us.")
-  }
-
-/*********RESET INPUTS***********/
-  $("input#name").val("");
-  $("input#mail").val("");
-  $("textarea#comment").val("");
-});
-});
+  
+})
